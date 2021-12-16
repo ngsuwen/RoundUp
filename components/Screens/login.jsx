2b7851b042f4f2b646c6daebc18331bfe67ac072
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-12-16 14:46:22 +0800

    FE login page done

---
+++ components/Screens/login.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  FormControl,
+  Stack,
+  Input,
+  Heading,
+  Center,
+  NativeBaseProvider,
+  Button,
+  Text,
+  Pressable,
+} from "native-base";
+
+export default function About({ navigation }) {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        <Stack
+          space={2}
+          w={{
+            base: "75%",
+            md: "25%",
+          }}
+        >
+          <Center>
+            <Heading mb="5">Round Up</Heading>
+          </Center>
+          <FormControl isRequired>
+            <Input size="md" placeholder="username or email" />
+            <Input size="md" placeholder="password" />
+          </FormControl>
+          <Button small primary>
+            <Text>Log In</Text>
+          </Button>
+          <Center>
+            <Text>Don't have an account?
+                <Pressable onPress={() => navigation.navigate("About")}>
+                <Text color={"cyan.600"} fontWeight={'bold'}>  Sign up</Text>
+                </Pressable>
+            </Text>
+          </Center>
+        </Stack>
+      </Center>
+    </NativeBaseProvider>
+  );
+}
