be8d93e1aff8d4c46e4fdb4776490c0f1872fb2d
qamarularifin <qamarulafro89@gmail.com>
2021-12-21 21:10:40 +0800

    added submit button for entry page cash -not working

--- components/entryPage.jsx
+++ components/entryPage.jsx
@@ -1,23 +1,49 @@
 import React from 'react'
 import { StyleSheet, Text, TextInput, View, Image, SafeAreaView, Dimensions, Button } from 'react-native';
 
+const BACKEND_BASE_URL=process.env.REACT_APP_BACKEND_BASE_URL
+
 export default function EntryPage() {
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const res = await fetch(`${BACKEND_BASE_URL}/data/cash`, {
+          method: "POST",
+          body: JSON.stringify({ description: event.target.name.value }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        if(res.status!==200){
+          console.error('create data cash failed')
+        }
+        
+      }
+
+
     return (
         
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <View>
-                <TextInput type="text" placeholder='Name'  />   
-                <TextInput type="text" placeholder='Date'  />   
+                <TextInput 
+                    type="text" 
+                    name="name"
+                    placeholder='Enter Name'
+                      />   
+                <Button title="Submit" onPress={handleSubmit}></Button>
+
+
+                {/* <TextInput type="text" placeholder='Date'  />   
                 <TextInput type="text" placeholder='Amount'  /> 
                 <TextInput type="text" placeholder='Category'  /> 
-                <TextInput type="text" placeholder='Description'  /> 
+                <TextInput type="text" placeholder='Description'  />  */}
     
             </View>
 
           
 
 
-        </View>
+        </SafeAreaView>
             
         
     )
