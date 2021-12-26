27928a18147e706ad8a73c143c20ee8c24b6a18c
qamarularifin <qamarulafro89@gmail.com>
2021-12-26 12:23:07 +0800

    added category wheel

--- components/Screens/entryExpensePage.jsx
+++ components/Screens/entryExpensePage.jsx
@@ -1,22 +1,26 @@
 import React, {useState} from 'react';
-import { StyleSheet, TextInput, Text, View, Image, SafeAreaView, Dimensions, Button } from 'react-native';
+import { StyleSheet, TextInput,View, Picker, SafeAreaView, Button } from 'react-native';
 import DatePicker from "@react-native-community/datetimepicker"
+
 //const BACKEND_BASE_URL=process.env.REACT_APP_BACKEND_BASE_URL
 
-
+const category = ["Shopping", "Food", "Health", "Transportation", "Household"]
 const EntryExpensePage = () => {
 
    const [date, setDate] = useState(new Date())
    const [amount, setAmount] = useState();
-   const [category, setCategory] = useState("");
+   //const [category, setCategory] = useState("");
+   const [selectedValue, setSelectedValue] = useState("Shopping")
    const [description, setDescription] = useState("");
 
+   // Date Picker
    const onChangeDate = (event, selectedDate) =>{
      const currentDate = selectedDate || date;
      setDate(currentDate)
    }
 
 
+  
 
 
     const handleSubmit = async (event) => {
@@ -32,7 +36,7 @@ const EntryExpensePage = () => {
                 { 
                   date: date,
                   amount: amount,
-                  category: category,
+                  category: selectedValue,
                   description: description  }
                 
               ]      
@@ -51,7 +55,7 @@ const EntryExpensePage = () => {
         
       }
     return (
-        // work on username ref and date
+        // work on username ref 
         <SafeAreaView style={styles.container} >
             <View>
                 <DatePicker
@@ -67,14 +71,31 @@ const EntryExpensePage = () => {
                     value={amount}
                     onChangeText={(text) => setAmount(text)}
                       />   
-                <TextInput
+                {/* <TextInput
                     style={styles.textinput}
                     type="submit" 
                     name="category"
                     placeholder="Enter Category"
                     value={category}
                     onChangeText={(text) => setCategory(text)}
-                      />   
+                      />    */}
+        
+                
+                <Picker
+                  selectedValue={selectedValue}
+                  style={styles.picker}
+                  onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+                >
+                
+                  <Picker.Item label="Shopping" value="Shopping" />
+                  <Picker.Item label="Food" value="Food" />
+                  <Picker.Item label="Health" value="Health" />
+                  <Picker.Item label="Transportation" value="Transportation" />
+                  <Picker.Item label="Household" value="Household" />
+
+                </Picker>
+                
+
                 <TextInput
                     style={styles.textinput}
                     type="submit" 
@@ -82,7 +103,9 @@ const EntryExpensePage = () => {
                     placeholder="Enter Description"
                     value={description}
                     onChangeText={(text) => setDescription(text)}
-                      />   
+                      /> 
+                
+                
                 <Button title="Submit" onPress={handleSubmit} />
 
 
@@ -103,17 +126,29 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        
     },
     datepicker:{
       paddingVertical: 10,
       paddingHorizontal: 10,
+      // borderColor: "gray",
+      // borderWidth: 1,
+      right: 100,
+      
+    },
+    textinput:{
+      paddingVertical: 20,
+      paddingHorizontal: 100,
+      marginTop: 10,
+      marginBottom: 10,
       borderColor: "gray",
       borderWidth: 1,
     },
-    textinput:{
-      paddingVertical: 10,
-      paddingHorizontal: 10,
-      borderColor: "gray",
-      borderWidth: 1,
+    picker:{
+      justifyContent: "center",
+      // left: 60,
+   
+
+      
     }
 })
