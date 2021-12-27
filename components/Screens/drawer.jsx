596c1bedf84d5acf9d1d41cbc6ef2816a3ace533
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-12-28 01:50:12 +0800

    logout will bring user back to login page

--- components/Screens/drawer.jsx
+++ components/Screens/drawer.jsx
@@ -1,57 +1,72 @@
 import * as React from "react";
-import { Button, Pressable, View } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
 import Home from "./home";
 import About from "./about";
 import Generalpage from "./generalPage";
-import EntryExpensePage from "./entryExpensePage"
-import IndexExpensePage from "./indexExpensePage"
-import ShowExpensePage from "./showExpensePage"
+import EntryExpensePage from "./entryExpensePage";
+import IndexExpensePage from "./indexExpensePage";
+import ShowExpensePage from "./showExpensePage";
 import EditExpensePage from "./editExpensePage";
-import Profile from "./profile"
+import Profile from "./profile";
 import logoutApi from "../api/logoutApi";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { UserContext } from "../../App";
 
 const Drawer = createDrawerNavigator();
 
-export default function App() {
-  const [user, setUser] = React.useContext(UserContext)
+export default function App({ navigation }) {
+  const [user, setUser] = React.useContext(UserContext);
 
-  async function logoutHandler(){
+  async function logoutHandler() {
     try {
-      const refreshToken = await AsyncStorage.getItem('refreshToken')
-      await logoutApi(refreshToken)
-      await AsyncStorage.removeItem('accessToken')
-      await AsyncStorage.removeItem('refreshToken')
-      setUser('')
-      console.log('cleared')
+      const refreshToken = await AsyncStorage.getItem("refreshToken");
+      await logoutApi(refreshToken);
+      await AsyncStorage.removeItem("accessToken");
+      await AsyncStorage.removeItem("refreshToken");
+      setUser("");
+      console.log("cleared");
+      navigation.navigate("Login");
     } catch (err) {
-      console.log('error')
+      console.log("error");
     }
-    return
+    return;
+  }
+
+  function CustomDrawerContent(props) {
+    return (
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+        <DrawerItem label="Logout" onPress={logoutHandler} />
+      </DrawerContentScrollView>
+    );
   }
 
   return (
-    
-      <Drawer.Navigator initialRouteName="Home" screenOptions={{
-        headerShown:true, 
-        headerStyle:{
-          backgroundColor:'white',
-          elevation:0,
-          shadowOpacity:0
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: "white",
+          elevation: 0,
+          shadowOpacity: 0,
         },
-        }}>
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="About" component={About} />
-        <Drawer.Screen name="General Page" component={Generalpage} />
-        <Drawer.Screen name="Entry Expense Page" component={EntryExpensePage} />
-        <Drawer.Screen name="Index Expense Page" component={IndexExpensePage} />
-        <Drawer.Screen name="Show Expense Page" component={ShowExpensePage} />
-        <Drawer.Screen name="Edit Expense Page" component={EditExpensePage} />
-        <Drawer.Screen name="Profile" component={Profile} />
-        <Drawer.Screen name="Logout" component={Home} initialParams={{ post: logoutHandler }}/>
-      </Drawer.Navigator>
+      }}
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="About" component={About} />
+      <Drawer.Screen name="General Page" component={Generalpage} />
+      <Drawer.Screen name="Entry Expense Page" component={EntryExpensePage} />
+      <Drawer.Screen name="Index Expense Page" component={IndexExpensePage} />
+      <Drawer.Screen name="Show Expense Page" component={ShowExpensePage} />
+      <Drawer.Screen name="Edit Expense Page" component={EditExpensePage} />
+      <Drawer.Screen name="Profile" component={Profile} />
+    </Drawer.Navigator>
   );
 }
