cca16511207c698b15b776d9dff7549c429b90a1
Shi Zheng <kangshizheng@hotmail.com>
2021-12-28 21:21:07 +0800

    added placeholder for month with no data

--- components/Charts/expenseLineChart.jsx
+++ components/Charts/expenseLineChart.jsx
@@ -11,56 +11,33 @@ const _ = require('underscore')
 
 export default function expenseLineChartComponent() {
 
-  const screenWidth = Dimensions.get('screen').width
-  const screenHeight = Dimensions.get('screen').height
+const screenWidth = Dimensions.get('screen').width
+const screenHeight = Dimensions.get('screen').height
 
-  const chartConfig = {
-    backgroundGradientFrom: "#FFFFFF",
-    backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: "#FFFFFF",
-    backgroundGradientToOpacity: 0.5,
-    color: (opacity = 1) => `rgba(163, 71, 165, ${opacity})`,
-    strokeWidth: 2, // optional, default 3
-    barPercentage: 0.5,
-    useShadowColorFromDataset: false // optional
-  }
+const chartConfig = {
+backgroundGradientFrom: "#FFFFFF",
+backgroundGradientFromOpacity: 0,
+backgroundGradientTo: "#FFFFFF",
+backgroundGradientToOpacity: 0.5,
+color: (opacity = 1) => `rgba(163, 71, 165, ${opacity})`,
+strokeWidth: 2, // optional, default 3
+barPercentage: 0.5,
+useShadowColorFromDataset: false // optional
+}
+
+const {expenseMonthContext,expenseMonthGroupedByDate} = useContext(DataContext)
+const [fetchedExpenseEntries,setFetchedExpenseEntries] = expenseMonthContext
+const [allLabels,setAllLabels] = useState([])
+const [dataPoints,setDataPoints] = useState([0])
 
 
-  const {expenseMonthContext,expenseMonthGroupedByDate} = useContext(DataContext);
-  const [fetchedExpenseEntries,setFetchedExpenseEntries] = expenseMonthContext
- 
-  
+useEffect(()=>{
+reloadExpenses()
+},[fetchedExpenseEntries])
+
+
+const reloadExpenses = () => {
 // grouping logic
-// const entriesByDay = _(fetchedExpenseEntries).groupBy((element)=>{
-//   const groupedDate = element.expensesentry.date
-//   const formattedGroupedDate = moment(groupedDate, moment.ISO_8601).format('YYYY-MM-DD')
-//   return formattedGroupedDate
-// })
-
-// console.log('entriesbyday:',entriesByDay)
-// const allDates = Object.keys(entriesByDay)
-// const entries = allDates.map((date,index)=>{
-
-// // method for calculating total amount for each day
-// let totalAmount = 0
-// entriesByDay[date].forEach((entry)=>{
-//     totalAmount += entry.expensesentry.amount
-// })
-
-// const linedata = {
-//     labels: ['1','2','3'],
-//     datasets: [
-//       {
-//         data: [100,200,300],
-//         color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-//         strokeWidth: 3 // optional
-//       }
-//     ],
-//     legend: ["Expense"] // optional
-//   }
-
-
-  // grouping logic
 const entriesByDay = _(fetchedExpenseEntries).groupBy((element)=>{
   const groupedDate = element.expensesentry.date
   const formattedGroupedDate = moment(groupedDate, moment.ISO_8601).format('DD')
@@ -69,23 +46,37 @@ const entriesByDay = _(fetchedExpenseEntries).groupBy((element)=>{
 
 // console.log('entriesbyday:',entriesByDay)
 const allDates = Object.keys(entriesByDay).sort((a,b)=>a-b) // -> this is labels
+setAllLabels(allDates)
 
-const dataPoints = []
-
-allDates.forEach((date)=>{
+const totalAmountArr = allDates.map((date)=>{
 // method for calculating total amount for each day
 let totalAmount = 0
 entriesByDay[date].forEach((entry)=>{
 totalAmount += entry.expensesentry.amount
 })
-dataPoints.push(totalAmount)
+return totalAmount
 })
 
+console.log('totalamountarr:',totalAmountArr)
+
+// this condition is required because if array is empty, react-chart-kit will return invalid number error
+if(totalAmountArr.length>0){
+  setDataPoints(totalAmountArr)
+}else{
+  setDataPoints([0])
+  setAllLabels(['No Data Available'])
+}
+
+
+
+}
+
+
 const linedata = {
-  labels: allDates,
+  labels: allLabels,
   datasets: [
     {
-      data: dataPoints,
+      data: dataPoints?dataPoints:[0],
       color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
       strokeWidth: 3 // optional
     }
@@ -93,32 +84,16 @@ const linedata = {
   legend: ["Expense"] // optional
 }
 
-// useEffect(()=>{
-//   const linedata = entriesByDay[date].map((entry,index)=>{
-//     return {
-//       labels: moment(entry.expenseentry.date, moment.ISO_8601).format('DD-MMM'),
-//       datasets: [
-//         {
-//           data: element.expenseentry.amount,
-//           color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-//           strokeWidth: 3 // optional
-//         }
-//       ],
-//       legend: ["Expense"] // optional
-//     }
-//   })
 
-// },[fetchedExpenseEntries])
-
-  return (
-        <LineChart
-            data={linedata}
-            width={screenWidth}
-            height={screenHeight*0.2}
-            chartConfig={chartConfig}
-            bezier
-        />
-  )
+return (
+      <LineChart
+          data={linedata}
+          width={screenWidth}
+          height={screenHeight*0.2}
+          chartConfig={chartConfig}
+          bezier
+      />
+)
 }
 
 
