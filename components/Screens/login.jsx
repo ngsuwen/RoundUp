01f7a45b45826784b107c3b2b8e0727db2b4c11b
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-12-29 23:19:57 +0800

    added keyboardavoidingview

--- components/Screens/login.jsx
+++ components/Screens/login.jsx
@@ -9,6 +9,7 @@ import {
   Button,
   Text,
   Pressable,
+  KeyboardAvoidingView,
   Icon,
 } from "native-base";
 import { Image } from "react-native";
@@ -16,7 +17,7 @@ import { Entypo } from "@expo/vector-icons";
 import loginAuth from "../api/loginAuth";
 import getUserId from "../api/getUserId";
 import DataContext from "../../context/DataContext";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function LoginPage({ navigation }) {
   // useState
@@ -25,8 +26,8 @@ export default function LoginPage({ navigation }) {
   const [username, setUsername] = useState("");
 
   // useContext
-  const { userContext } = useContext(DataContext)
-  const [user, setUser]=userContext
+  const { userContext } = useContext(DataContext);
+  const [user, setUser] = userContext;
 
   // password state
   const [show, setShow] = useState(false);
@@ -38,82 +39,94 @@ export default function LoginPage({ navigation }) {
     setIsLoginValid(
       checkUserAuth.error
         ? checkUserAuth.error == "invalid user"
-        ? "invalid user"
-        : "invalid password"
+          ? "invalid user"
+          : "invalid password"
         : "pass"
     );
     try {
       // store tokens in FE
-      await AsyncStorage.setItem('accessToken',checkUserAuth.accessToken);
-      await AsyncStorage.setItem('refreshToken',checkUserAuth.refreshToken);
-      const userId = await getUserId(checkUserAuth.refreshToken)
-      setUser(userId)
-      console.log(userId)
+      await AsyncStorage.setItem("accessToken", checkUserAuth.accessToken);
+      await AsyncStorage.setItem("refreshToken", checkUserAuth.refreshToken);
+      const userId = await getUserId(checkUserAuth.refreshToken);
+      setUser(userId);
+      console.log(userId);
       navigation.navigate("Drawer");
-    } catch(err) {
-      console.log(err)
+    } catch (err) {
+      console.log(err);
       navigation.navigate("Login");
     }
   }
 
   return (
     <NativeBaseProvider>
-      <Center flex={1} px="3">
-        <Stack
-          space={2}
-          w={{
-            base: "75%",
-            md: "25%",
-          }}
-        >
-          <Center>
-            <Image source={require("../../assets/rounduplogo.png")} />
-            <Heading mb="5">Round Up</Heading>
-            <Text color={"red.600"}>
-              {isLoginValid == "pass"
-                ? ""
-                : isLoginValid == "invalid user"
-                ? "invalid user"
-                : "invalid password"}
-            </Text>
-          </Center>
-          <FormControl isRequired>
-            <Input
-              size="md"
-              placeholder="username or email"
-              onChangeText={(text) => setUsername(text)}
-            />
-            <Input
-              type={show ? "text" : "password"}
-              size="md"
-              onChangeText={(text) => setPassword(text)}
-              InputRightElement={
-                <Pressable onPress={passwordHandler}>
-                  <Icon
-                    as={Entypo}
-                    name={show ? "eye" : "eye-with-line"}
-                    color="coolGray.600"
-                    size={5}
-                    marginRight={3}
-                  />
-                </Pressable>
-              }
-              placeholder="password"
-            />
-          </FormControl>
-          <Button onPress={loginHandler} small primary>
-            <Text>Log In</Text>
-          </Button>
-          <Center>
-            <Text>
-              Don't have an account?{" "}
-                <Text color={"cyan.600"} fontWeight={"bold"} onPress={() => navigation.navigate("Sign Up")}>
+      <KeyboardAvoidingView
+        h={{
+          base: "100%",
+          lg: "auto",
+        }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <Center flex={1} px="3">
+          <Stack
+            space={2}
+            w={{
+              base: "75%",
+              md: "25%",
+            }}
+          >
+            <Center>
+              <Image source={require("../../assets/rounduplogo.png")} />
+              <Heading mb="5">Round Up</Heading>
+              <Text color={"red.600"}>
+                {isLoginValid == "pass"
+                  ? ""
+                  : isLoginValid == "invalid user"
+                  ? "invalid user"
+                  : "invalid password"}
+              </Text>
+            </Center>
+            <FormControl isRequired>
+              <Input
+                size="md"
+                placeholder="username or email"
+                onChangeText={(text) => setUsername(text)}
+              />
+              <Input
+                type={show ? "text" : "password"}
+                size="md"
+                onChangeText={(text) => setPassword(text)}
+                InputRightElement={
+                  <Pressable onPress={passwordHandler}>
+                    <Icon
+                      as={Entypo}
+                      name={show ? "eye" : "eye-with-line"}
+                      color="coolGray.600"
+                      size={5}
+                      marginRight={3}
+                    />
+                  </Pressable>
+                }
+                placeholder="password"
+              />
+            </FormControl>
+            <Button onPress={loginHandler} small primary>
+              <Text>Log In</Text>
+            </Button>
+            <Center>
+              <Text>
+                Don't have an account?{" "}
+                <Text
+                  color={"cyan.600"}
+                  fontWeight={"bold"}
+                  onPress={() => navigation.navigate("Sign Up")}
+                >
                   Sign up
-                </Text>         
-            </Text>
-          </Center>
-        </Stack>
-      </Center>
+                </Text>
+              </Text>
+            </Center>
+          </Stack>
+        </Center>
+      </KeyboardAvoidingView>
     </NativeBaseProvider>
   );
 }
