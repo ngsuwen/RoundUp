6a7fbf4e9b1c804bd614b4bbc4247f9aa40c4bbc
Shi Zheng <kangshizheng@hotmail.com>
2022-01-04 22:15:35 +0800

    ticker page is now able to calculate cost basis, total amount paid, current stock qty and unrealized P/L of each ticker

--- components/Accordion/investmentAccordion.jsx
+++ components/Accordion/investmentAccordion.jsx
@@ -1,5 +1,6 @@
-// category in accordion box? 
 // new entry not opened up after creation (bonus)
+// why is tickerandprice state rerendered?  and investmentaccordion page is not rerendered? 
+// for this page, we cannot fetch investment record per month as we will need all data to calculate p/l. Hence we shall only filter all the data per month selected. 
 
 import React from 'react';
 import { useState,useEffect, useContext } from 'react'
@@ -65,28 +66,89 @@ const RenderTransactionHistory = () => {
   const allDatesAscending = Object.keys(entriesByDay).sort()
   const allDates = allDatesAscending.reverse()
 
-  let totalStockQty = 0
-  let totalAmtPaid = 0
-  const entries = allDates.map((date,index)=>{
-  // method for calculating total stock qty & total amount paid for each day
-  entriesByDay[date].forEach((entry)=>{
-      if(entry.investmentsentry.transaction === 'Buy'){
-        totalStockQty += entry.investmentsentry.quantity
-      }
-      if(entry.investmentsentry.transaction === 'Sell'){
-        totalStockQty -= entry.investmentsentry.quantity    
-      }
-  })
-  })
-  const monthOfExpense = moment(investmentMonth, moment.ISO_8601).format('YYYY-MM')
+// avg price per stock, needed to find new totalAmountPaid
+// cost basis for one BUY transaction can be derived by price bought * qty
+// to get a moving cost basis, we need to get the average of all costBasis up to that buy transaction
+
+let costBasis = 0
+
+// for every SELL txn, we need minus costBasis * qty sold off from total amount paid to find total amount paid for remaining qty of shares
+let totalAmountPaid = 0
+
+let currentStockQty = 0
+
+
+///// TO FIND UNREALIZED P/L /////
+
+// HANDLING SELL/BUY TXN
+// for every SELL txn:
+// 1) get new totalAmountPaid [totalAmountPaid -= qtysold * current costBasis till that transaction]
+// 2) update total qty of stocks left [currentStockQty -= qtysold]
+// cost basis does not change when you sell because ratio of totalAmountPaid being minus/totalqty decreased will be a constant 
+
+// for every BUY txn:
+// 1) update totalAmountPaid [totalAmountPaid += buy in price * qty bought]
+// 2) update total qty of stocks left [currentStockQty += qtybought]
+// 3) update cost basis [totalAmountPaid/currentStockQty]
+
+// Finally to get unrealized P/L, 
+// loop through the whole list in chronological order and then use the formula as follows: 
+// unrealizedPL = currentStockQty*currentprice - totalAmountPaid
+
+const entries = allDates.map((date,index)=>{
+
+entriesByDay[date].forEach((entry,index)=>{ // will have to use loop instead of (for txn of arr) in order to access the index. index is needed to single out the first buy in price to not get the average of the first buy in price. index 1 onwards will have to be divided by 2, but not index 0. 
+console.log('entriesbydate:',entriesByDay[date])
+ if(entry.investmentsentry.transaction==='Buy'){
+    totalAmountPaid+=entry.investmentsentry.price*entry.investmentsentry.quantity
+    currentStockQty+=entry.investmentsentry.quantity
+    
+    // update cost basis after totalAmountPaid and currentStockQty is updated 
+    if (index === 0){
+        costBasis=entry.investmentsentry.price
+    }
+    else{
+        costBasis=totalAmountPaid/currentStockQty // this statement is not true for the first transaction 
+    }
+ }
+ if(entry.investmentsentry.transaction==='Sell'){
+    totalAmountPaid-=entry.investmentsentry.quantity*costBasis
+    currentStockQty-=entry.investmentsentry.quantity
+    totalamountreceived=entry.investmentsentry.quantity*entry.investmentsentry.price
+  }   
+})
+})
+
+// assuming current price is $20 (from API)
+let unrealizedPL = (currentStockQty*props.selectedTickerAndPrice.value)-totalAmountPaid
+
+console.log('costBasis:',costBasis)
+console.log('totalAmountPaid:',totalAmountPaid)
+console.log('currentstockqty:',currentStockQty)
+console.log('unrealized P/L:',unrealizedPL)
+
+
+
+
+// console.log('costBasis:',costBasis)
+// console.log('totalAmountPaid:',totalAmountPaid)
+// console.log('currentstockqty:',currentStockQty)
+// console.log('unrealized P/L:',unrealizedPL)
+
+// linking to cash page
+// for all sell transactions, to add into cash balance 
+// for all buy transactions, to deduct from cash balance 
+
+
+  // const monthOfExpense = moment(investmentMonth, moment.ISO_8601).format('YYYY-MM')
 
    
   // do this last when you have all info
-  setTickerData(
-    {'totalstockqty:':totalStockQty,
-     'totalamountpaid:':totalAmtPaid,
-     'totalprofits':totalProfits,
-    })
+  // setTickerData(
+  //   {'totalstockqty:':totalStockQty,
+  //    'totalAmountPaid:':totalAmtPaid,
+  //    'totalprofits':totalProfits,
+  //   })
 }
 
 useEffect(()=>{
