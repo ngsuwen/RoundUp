a9cccea30b6d21a350752317837c8b2b46930045
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2022-01-11 13:01:43 +0800

    linked yearlyInvestment to BE, priceHistory logic still needs to finalize

--- components/api/yearlyInvestment.js
+++ components/api/yearlyInvestment.js
@@ -1,21 +1,55 @@
 export default async function yearlyExpense(id, date) {
-    var raw = "";
-  
-    var requestOptions = {
-      method: "GET",
-      body: raw,
-      redirect: "follow",
-    };
-  
-    try {
-      const response = await fetch(
-        `https://roundup-api.herokuapp.com/data/expense/user/${id}/yearly/${date}`,
-        requestOptions
+  var raw = "";
+
+  var requestOptions = {
+    method: "GET",
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await fetch(
+      `https://roundup-api.herokuapp.com/data/investment/user/${id}/yearly/${date}`,
+      requestOptions
+    );
+    const data = await response.json();
+
+    let investmentArr = [];
+    const monthOfInvestmentDateObj = new Date(date)
+    // console.log("monthOfInvestmentDateObj", monthOfInvestmentDateObj)
+    for (let i = 1; i <= 12; i++) {
+      let monthlyInvestment = 0;
+      // last day of month
+      let lastday = new Date(
+        monthOfInvestmentDateObj.getFullYear() - 1,
+        monthOfInvestmentDateObj.getMonth() + i + 1,
+        1
       );
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      return err.json();
+      for (let entry of data[i-1]) {
+        // console.log("last day",lastday)
+        if (entry==null) {
+          monthlyInvestment += 0;
+        } else {
+          // if (i != 12) {
+          //   indexFound = entry.priceHistory.findIndex(
+          //     (element) => element.date == lastday.getTime() / 1000
+          //   );
+          //   monthlyInvestment +=
+          //     Number(entry.priceHistory[indexFound].price) *
+          //     Number(entry.priceHistory[indexFound].quantity);
+          //   //console.log(indexFound, lastday.getTime()/1000, entry._id)
+          // } else {
+            monthlyInvestment +=
+              Number(entry.priceHistory[entry.priceHistory.length - 1].price) *
+              Number(entry.priceHistory[entry.priceHistory.length - 1].quantity);
+          // }
+        }
+      }
+      investmentArr.push(monthlyInvestment.toFixed(2));
+      // console.log(investmentArr, lastday)
     }
+    return investmentArr;
+  } catch (err) {
+    return err.json();
   }
-  
+}
