fd968c27130ef7d702243b8dbe1db92751d3d86b
qamarularifin <qamarulafro89@gmail.com>
2022-01-12 23:32:22 +0800

    edited the validation for investment entry

--- components/Screens/Investment/entryInvestmentPage.jsx
+++ components/Screens/Investment/entryInvestmentPage.jsx
@@ -213,12 +213,13 @@ const EntryInvestmentPage = ({ navigation }) => {
           alert("One of the fields is invalid. Create failed!");
           return navigation.navigate("Entry Investment Page");
         }
-      // no field validation error
-      if (priceInvestment.length < 1 || qtyInvestment.length < 1) {
-        alert("One of the fields is empty. Create failed!");
-        return navigation.navigate("Entry Investment Page");
+        // no field validation error not working
+        if (priceInvestment.length < 1 || qtyInvestment.length < 1) {
+          alert("One of the fields is empty. Create failed!");
+          return navigation.navigate("Entry Investment Page");
+        }
       }
-      }
+      
       setExpenseForceRender(!expenseForceRender);
 
      
